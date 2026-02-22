a30478bbd54749c284cf47b9e23d6a3ab75a2f5f
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2026-02-22 12:15:50 +0530

    update swagger https

--- task-tracker-backend/src/main/java/com/miraisense/task_tracker_backend/config/OpenApiConfig.java
+++ task-tracker-backend/src/main/java/com/miraisense/task_tracker_backend/config/OpenApiConfig.java
@@ -5,26 +5,35 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class OpenApiConfig {
-    @Bean
-    public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "bearerAuth";
-        return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")))
-                .info(new Info()
-                        .title("Task Tracker API")
-                        .version("1.0")
-                        .description("API documentation for the Mini Task Tracker application"));
-    }
+
+        @Value("${app.server.url:http://localhost:8090}")
+        private String serverUrl;
+
+        @Bean
+        public OpenAPI customOpenAPI() {
+                final String securitySchemeName = "bearerAuth";
+                return new OpenAPI()
+                                .servers(List.of(new Server().url(serverUrl).description("Server")))
+                                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                                .components(new Components()
+                                                .addSecuritySchemes(securitySchemeName,
+                                                                new SecurityScheme()
+                                                                                .name(securitySchemeName)
+                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")))
+                                .info(new Info()
+                                                .title("Task Tracker API")
+                                                .version("1.0")
+                                                .description("API documentation for the Mini Task Tracker application"));
+        }
 }
